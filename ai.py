8183a074d2fb84c7401a70c6b16a78360c2c00ba
kokofixcomputers <github@kokofixcomputers.serv00.net>
2024-01-01 23:02:21 +0000

    Commit

--- ai.py
+++ ai.py
@@ -1,8 +1,8 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
 
 # Download the model to your local cache
-tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
-model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+tokenizer = AutoTokenizer.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
+model = AutoModelForTokenClassification.from_pretrained("mistralai/Mixtral-8x7B-Instruct-v0.1")
 nlp = pipeline("ner", model=model, tokenizer=tokenizer)
 
 # Run the model on a sample input
